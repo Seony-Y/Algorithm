9110ed16b3c296fd62c14d14752682fca55bd70f
Seony-Y <76675209+Seony-Y@users.noreply.github.com>
2025-10-26 15:35:27 +0900

    [Bronze I] Title: 평균, Time: 88 ms, Memory: 9704 KB -BaekjoonHub

---
+++ 백준/Bronze/1546. 평균/평균.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+const path = require("path");
+
+const src =
+  process.platform === "linux"
+    ? 0
+    : process.env.LOCAL_INPUT || path.join(__dirname, "input.txt");
+const raw = fs.readFileSync(src, "utf8").replace(/\r/g, "").trim();
+
+const lines = raw.split("\n"); // 줄 단위가 필요할 때
+const tokens = raw.split(/\s+/); // 공백 단위가 필요할 때
+const nums = tokens.map(Number); // 전부 숫자면 이 한 줄로도 OK
+
+// --- 여기부터 풀이 ---
+const N = nums[0];
+
+const arr = nums.slice(1, N + 1);
+const M = Math.max(...arr);
+const newArr = arr.map((x) => (x / M) * 100);
+const result = newArr.reduce((a, b) => a + b, 0) / N;
+
+console.log(result);

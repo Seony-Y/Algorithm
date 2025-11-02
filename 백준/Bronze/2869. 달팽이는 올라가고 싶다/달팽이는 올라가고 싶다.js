5e665ed187f3749b7cf196631ff2c8cdd29d0ddd
Seony-Y <76675209+Seony-Y@users.noreply.github.com>
2025-11-02 20:28:24 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 92 ms, Memory: 9340 KB -BaekjoonHub

---
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+const path = require("path");
+
+const src =
+  process.platform === "linux"
+    ? 0
+    : process.env.LOCAL_INPUT || path.join(__dirname, "input.txt");
+const raw = fs.readFileSync(src, "utf8").replace(/\r/g, "").trim();
+
+const lines = raw.split("\n"); // 줄 단위가 필요할 때
+const tokens = raw.split(/\s+/); // 공백 단위가 필요할 때
+const nums = tokens.map(Number); // 전부 숫자면 이 한 줄로도 OK
+
+const [A, B, V] = nums;
+
+let day = Math.ceil((V - B) / (A - B));
+
+console.log(day);

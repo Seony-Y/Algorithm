e59e113b7c43f965100ccccf67e3081fcb715fff
Seony-Y <76675209+Seony-Y@users.noreply.github.com>
2025-11-05 20:06:43 +0900

    [Bronze III] Title: 직사각형에서 탈출, Time: 92 ms, Memory: 9340 KB -BaekjoonHub

---
+++ 백준/Bronze/1085. 직사각형에서 탈출/직사각형에서 탈출.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+const path = require("path");
+
+const src =
+  process.platform === "linux"
+    ? 0
+    : process.env.LOCAL_INPUT || path.join(__dirname, "input.txt");
+const raw = fs.readFileSync(src, "utf8").replace(/\r/g, "").trim();
+
+const lines = raw.split("\n"); // 줄 단위가 필요할 때
+const tokens = raw.split(/\s+/); // 공백 단위가 필요할 때
+const nums = tokens.map(Number); // 전부 숫자면 이 한 줄로도 OK
+
+const [x, y, w, h] = nums;
+
+const distX = Math.min(x, w - x);
+const distY = Math.min(y, h - y);
+
+console.log(Math.min(distX, distY));

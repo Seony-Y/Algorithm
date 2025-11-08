e8f1930d84e5949d82ec014d6764b23ea0f59fe2
Seony-Y <76675209+Seony-Y@users.noreply.github.com>
2025-11-08 11:53:11 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 5, Time: 92 ms, Memory: 9340 KB -BaekjoonHub

---
+++ 백준/Bronze/24266. 알고리즘 수업 － 알고리즘의 수행 시간 5/알고리즘 수업 － 알고리즘의 수행 시간 5.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+const path = require("path");
+
+const src =
+  process.platform === "linux"
+    ? 0
+    : process.env.LOCAL_INPUT || path.join(__dirname, "input.txt");
+const raw = fs.readFileSync(src, "utf8").replace(/\r/g, "").trim();
+
+const lines = raw.split("\n"); // 줄 단위가 필요할 때
+const tokens = raw.split(/\s+/); // 공백 단위가 필요할 때
+const nums = tokens.map(Number); // 전부 숫자면 이 한 줄로도 OK
+
+// MenOfPassion(A[], n) {
+//     sum <- 0;
+//     for i <- 1 to n
+//         for j <- 1 to n
+//             for k <- 1 to n
+//                 sum <- sum + A[i] × A[j] × A[k]; # 코드1
+//     return sum;
+// }
+
+const n = BigInt(nums[0])
+console.log((n ** 3n).toString());
+console.log(3);

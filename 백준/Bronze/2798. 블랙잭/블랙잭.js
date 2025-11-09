1d08152cab989a4ef250acd2e5af0256f21f034e
Seony-Y <76675209+Seony-Y@users.noreply.github.com>
2025-11-09 22:04:57 +0900

    [Bronze II] Title: 블랙잭, Time: 148 ms, Memory: 9624 KB -BaekjoonHub

---
+++ 백준/Bronze/2798. 블랙잭/블랙잭.js
@@ -0,0 +1,32 @@
+const fs = require("fs");
+const path = require("path");
+
+const src =
+  process.platform === "linux"
+    ? 0
+    : process.env.LOCAL_INPUT || path.join(__dirname, "input.txt");
+const raw = fs.readFileSync(src, "utf8").replace(/\r/g, "").trim();
+
+const lines = raw.split("\n"); // 줄 단위가 필요할 때
+const tokens = raw.split(/\s+/); // 공백 단위가 필요할 때
+const nums = tokens.map(Number); // 전부 숫자면 이 한 줄로도 OK
+
+function solution(nums) {
+  const [N, M] = nums.splice(0, 2);
+  let answer = 0;
+
+  for (let i = 0; i < N - 2; i++) {
+    for (let j = i + 1; j < N - 1; j++) {
+      for (let k = j + 1; k < N; k++) {
+        const sum = nums[i] + nums[j] + nums[k];
+        if (sum <= M && sum > answer) {
+          answer = sum;
+        }
+      }
+    }
+  }
+
+  return answer;
+}
+
+console.log(solution(nums));

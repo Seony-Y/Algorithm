58b924a36f470cff02eeeb0d385d14aa5a9fe7a0
Seony-Y <76675209+Seony-Y@users.noreply.github.com>
2025-11-10 18:32:10 +0900

    [Silver V] Title: 영화감독 숌, Time: 376 ms, Memory: 23264 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+const path = require("path");
+
+const src =
+  process.platform === "linux"
+    ? 0
+    : process.env.LOCAL_INPUT || path.join(__dirname, "input.txt");
+const raw = fs.readFileSync(src, "utf8").replace(/\r/g, "").trim();
+
+const lines = raw.split("\n"); // 줄 단위가 필요할 때
+const tokens = raw.split(/\s+/); // 공백 단위가 필요할 때
+const nums = tokens.map(Number); // 전부 숫자면 이 한 줄로도 OK
+
+const N = nums[0];
+let count = 0;
+let num = 665; 
+
+while (true) {
+  num++;
+  if (num.toString().includes("666")) {
+    count++;
+    if (count === N) {
+      console.log(num);
+      break;
+    }
+  }
+}

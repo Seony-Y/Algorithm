9346bbf2303542ab851c483da858a8f1fb94648e
Seony-Y <76675209+Seony-Y@users.noreply.github.com>
2025-12-10 22:57:48 +0900

    [Silver III] Title: 소수 구하기, Time: 2832 ms, Memory: 17388 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+const path = require("path");
+
+const src =
+  process.platform === "linux"
+    ? 0
+    : process.env.LOCAL_INPUT || path.join(__dirname, "input.txt");
+const raw = fs.readFileSync(src, "utf8").replace(/\r/g, "").trim();
+
+const lines = raw.split("\n"); // 줄 단위가 필요할 때
+const tokens = raw.split(/\s+/); // 공백 단위가 필요할 때
+const nums = tokens.map(Number); // 전부 숫자면 이 한 줄로도 OK
+
+const [N, M] = nums;
+
+function isPrime(num) {
+  if (num < 2) return false;
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) return false;
+  }
+  return true;
+}
+
+for (let i = N; i <= M; i++) {
+  if (isPrime(i)) {
+    console.log(i);
+  }
+}   

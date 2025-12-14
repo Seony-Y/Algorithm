6a1ff6ee30abfce67d0cabf82047a9dbfd1481d7
Seony-Y <76675209+Seony-Y@users.noreply.github.com>
2025-12-14 16:27:11 +0900

    [Bronze III] Title: 팩토리얼, Time: 92 ms, Memory: 9600 KB -BaekjoonHub

---
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+const path = require("path");
+
+const src =
+  process.platform === "linux"
+    ? 0
+    : process.env.LOCAL_INPUT || path.join(__dirname, "input.txt");
+const raw = fs.readFileSync(src, "utf8").replace(/\r/g, "").trim();
+
+const lines = raw.split("\n"); 
+const tokens = raw.split(/\s+/);
+const nums = tokens.map(Number);
+
+const N = nums[0];
+let result = 1;
+for (let i = 2; i <= N; i++) {
+  result *= i;
+}
+
+console.log(result);

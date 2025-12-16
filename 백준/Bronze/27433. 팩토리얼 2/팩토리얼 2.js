c08f43a6a99c5bc96715a7df10b585b123a121f9
Seony-Y <76675209+Seony-Y@users.noreply.github.com>
2025-12-16 20:29:53 +0900

    [Bronze V] Title: 팩토리얼 2, Time: 96 ms, Memory: 9588 KB -BaekjoonHub

---
+++ 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+const path = require("path");
+
+const src =
+  process.platform === "linux"
+    ? 0
+    : process.env.LOCAL_INPUT || path.join(__dirname, "input.txt");
+const raw = fs.readFileSync(src, "utf8").replace(/\r/g, "").trim();
+
+const lines = raw.split("\n"); 
+const tokens = raw.split(/\s+/);
+const nums = tokens.map(Number);
+
+const n = nums[0];
+
+function factorial(num) {
+    if (num === 0) {
+        return 1;
+    }
+    return num * factorial(num - 1);
+}
+
+console.log(factorial(n));
